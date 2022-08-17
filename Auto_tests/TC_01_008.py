8523d95632631d5e9181cafad30ad101ecd76e2a
radek541 <110462831+radek541@users.noreply.github.com>
2022-08-17 11:57:28 +0200

    v4

---
+++ Auto_tests/TC_01_008.py
@@ -0,0 +1,108 @@
+from time import sleep
+from login_password import login_standard
+from login_password import password_login
+from selenium.webdriver.common.by import By
+from lib2to3.pgen2 import driver
+from selenium import webdriver
+
+#Open webbrowser
+driver = webdriver.Edge()
+driver.maximize_window()
+driver.get('https://www.saucedemo.com/')
+
+#Login to shop
+login = driver.find_element(By.ID, 'user-name')
+login.click()
+login.send_keys(login_standard)
+sleep(0.5)
+
+password = driver.find_element(By.ID, 'password')
+password.click()
+password.send_keys(password_login)
+sleep(0.5)
+
+login_button = driver.find_element(By.ID, 'login-button')
+login_button.click()
+
+#Open product by name, add to cart and go to cart
+item_01 = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div/div[1]/div[1]/a/img')
+item_01.click()
+sleep(1)
+add_to_cart_button = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div[2]/button')
+add_to_cart_button.click()
+sleep(1)
+cart_button = driver.find_element(By.ID, 'shopping_cart_container')
+cart_button.click()
+sleep(1)
+continue_shoping = driver.find_element(By.ID, 'continue-shopping')
+continue_shoping.click()
+sleep(1)
+
+item_02 = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div/div[2]/div[1]/a/img')
+item_02.click()
+sleep(1)
+add_to_cart_button = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div[2]/button')
+add_to_cart_button.click()
+sleep(1)
+cart_button = driver.find_element(By.ID, 'shopping_cart_container')
+cart_button.click()
+sleep(1)
+continue_shoping = driver.find_element(By.ID, 'continue-shopping')
+continue_shoping.click()
+sleep(1)
+
+item_03 = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div/div[3]/div[1]/a/img')
+item_03.click()
+sleep(1)
+add_to_cart_button = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div[2]/button')
+add_to_cart_button.click()
+sleep(1)
+cart_button = driver.find_element(By.ID, 'shopping_cart_container')
+cart_button.click()
+sleep(1)
+continue_shoping = driver.find_element(By.ID, 'continue-shopping')
+continue_shoping.click()
+sleep(1)
+
+item_04 = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div/div[4]/div[1]/a/img')
+item_04.click()
+sleep(1)
+add_to_cart_button = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div[2]/button')
+add_to_cart_button.click()
+sleep(1)
+cart_button = driver.find_element(By.ID, 'shopping_cart_container')
+cart_button.click()
+sleep(1)
+continue_shoping = driver.find_element(By.ID, 'continue-shopping')
+continue_shoping.click()
+sleep(1)
+
+item_05 = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div/div[5]/div[1]/a/img')
+item_05.click()
+sleep(1)
+add_to_cart_button = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div[2]/button')
+add_to_cart_button.click()
+sleep(1)
+cart_button = driver.find_element(By.ID, 'shopping_cart_container')
+cart_button.click()
+sleep(1)
+continue_shoping = driver.find_element(By.ID, 'continue-shopping')
+continue_shoping.click()
+sleep(1)
+
+item_06 = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div/div[6]/div[1]/a/img')
+item_06.click()
+sleep(1)
+add_to_cart_button = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div[2]/button')
+add_to_cart_button.click()
+sleep(1)
+cart_button = driver.find_element(By.ID, 'shopping_cart_container')
+cart_button.click()
+sleep(1)
+continue_shoping = driver.find_element(By.ID, 'continue-shopping')
+continue_shoping.click()
+sleep(1)
+
+#Close webbrowser
+sleep(3)
+driver.close()
---
+++ Auto_tests/TC_01_009.py
@@ -0,0 +1,54 @@
+from time import sleep
+from login_password import login_standard
+from login_password import password_login
+from selenium.webdriver.common.by import By
+from lib2to3.pgen2 import driver
+from selenium import webdriver
+
+#Open webbrowser
+driver = webdriver.Edge()
+driver.maximize_window()
+driver.get('https://www.saucedemo.com/')
+
+#Login to shop
+login = driver.find_element(By.ID, 'user-name')
+login.click()
+login.send_keys(login_standard)
+sleep(0.5)
+
+password = driver.find_element(By.ID, 'password')
+password.click()
+password.send_keys(password_login)
+sleep(0.5)
+
+login_button = driver.find_element(By.ID, 'login-button')
+login_button.click()
+
+#Add products to cart in main site
+item_01 = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[2]/button')
+item_01.click()
+sleep(0.5)
+
+item_02 = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div/div[2]/div[2]/div[2]/button')
+item_02.click()
+sleep(0.5)
+
+item_03 = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div/div[3]/div[2]/div[2]/button')
+item_03.click()
+sleep(0.5)
+
+item_04 = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div/div[4]/div[2]/div[2]/button')
+item_04.click()
+sleep(0.5)
+
+item_05 = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div/div[5]/div[2]/div[2]/button')
+item_05.click()
+sleep(0.5)
+
+item_06 = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div/div/div[6]/div[2]/div[2]/button')
+item_06.click()
+sleep(0.5)
+
+#Close webbrowser
+sleep(3)
+driver.close()
